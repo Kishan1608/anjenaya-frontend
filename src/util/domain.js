0ac93f038a7edaa81cb9844d724eab711470a072
Kishan Chauhan <57287914+Kishan1608@users.noreply.github.com>
2023-03-23 15:25:07 +0530

    Update domain.js

--- src/util/domain.js
+++ src/util/domain.js
@@ -1,4 +1,4 @@
 export default process.env.NODE_ENV === 'development' 
-? "http://13.234.158.86/:5000" 
+? "http://localhost:5000" 
 : process.env.NODE_ENV === 'production' 
 && 'https://anjenaya-wealth-management.onrender.com';
